24a118c98ea74079af21afae4203d6cec8a40e9a
MateoFontaine <Mateofontaine7@gmail.com>
2024-10-25 17:28:54 -0300

    Empezamos a agergar [id] para cada perro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
